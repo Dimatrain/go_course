8e325ed367a3fdff7ca95f3cf5ac3fd538f9da92
Dmitry Pozdeev <dimatrain@gmail.com>
2021-02-05 03:48:12 +0300

    DAY-3 - задание третьего дня (#3)
    Co-authored-by: Дмитрий Поздеев <d.pozdeev@carprice.ru>

--- cmd/third/main.go
+++ cmd/third/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"io/ioutil"
+	"net/http"
+)
 
+func handler (res http.ResponseWriter, req *http.Request) {
+	if http.MethodPost == req.Method {
+		reqBody, _ := ioutil.ReadAll(req.Body)
+		setHeaders(res, req)
+		res.Write(reqBody)
+	} else {
+		res.Write([]byte("Please, choose POST method."))
+	}
+}
+
+func setHeaders(res http.ResponseWriter, req *http.Request) {
+	reqHeaders := req.Header
+	for key, values := range reqHeaders {
+		for _, value := range values {
+			res.Header().Set(key, value)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/post", handler)
+	http.ListenAndServe(":8080", nil)
 }
